75e65deaa9339a3d40fff4c1098eed6165234eae
alvin <alvinecom2018@gmail.com>
2025-02-26 22:11:31 +0700

    --

--- src/components/profile/Section1.tsx
+++ src/components/profile/Section1.tsx
@@ -99,12 +99,12 @@ const Section1 = () => {
         ) : (
           ""
         )}
-        <button
+        {/* <button
           onClick={() => handleLogout()}
           className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded mb-6"
         >
           Logout
-        </button>
+        </button> */}
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         <div>
@@ -136,6 +136,7 @@ const Section1 = () => {
           <input
             type="email"
             value={profile.email}
+            disabled={profile?.is_google ? true : false}
             onChange={(e) => setProfile({ ...profile, email: e.target.value })}
             placeholder="Enter email"
             className="w-full border border-gray-300 p-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
